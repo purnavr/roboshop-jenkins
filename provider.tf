22bb3a8642258361f09374b7d617891fd42b2312
purna <purnaraomba@gmail.com>
2024-05-03 15:40:29 +0530

    Roboshop Update

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
 # Configure the Jenkins Provider
+terraform {
+  required_providers {
+    jenkins = {
+      source = "taiidani/jenkins"
+      version = "0.10.2"
+    }
+  }
+}
+
+
 provider "jenkins" {
   server_url = "http://3.81.84.229:8080/"
   username   = data.aws_ssm_parameter.jenkins_user.value
