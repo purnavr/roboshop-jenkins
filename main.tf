b0aa19a21812fe44796a3848146ee1016f7fcb3e
purna <purnaraomba@gmail.com>
2024-05-04 14:50:18 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ resource "jenkins_folder" "folders" {
   name = var.folders[count.index]
 }
 
-resource "jenkins_job" "job" {
-  count = length(var.jobs)
-  name     = lookup(element(var.jobs, count.index ), "name", null )
-  folder   = lookup(element(var.jobs, count.index ), "folder", null )
-  template = templatefile("${path.module}/sb-job.xml", {
-    repo_url = lookup(element(var.jobs, count.index ), "repo_url", null )
-  })
-}
+#resource "jenkins_job" "job" {
+#  count = length(var.jobs)
+#  name     = lookup(element(var.jobs, count.index ), "name", null )
+#  folder   = lookup(element(var.jobs, count.index ), "folder", null )
+#  template = templatefile("${path.module}/sb-job.xml", {
+#    repo_url = lookup(element(var.jobs, count.index ), "repo_url", null )
+#  })
+#}
 
 resource "jenkins_job" "job" {
   count = length(var.jobs)
