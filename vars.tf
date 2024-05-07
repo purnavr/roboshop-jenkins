ab137cd9e57aa5bc23afbddc6f558812b54e7500
purna <purnaraomba@gmail.com>
2024-05-07 10:54:34 +0530

    Roboshop Update

--- vars.tf
+++ vars.tf
@@ -4,12 +4,12 @@ variable "folders" {
 
 variable "jobs" {
   default = [
-#    { name = "roboshop", folder = "infra", repo_url = "https://github.com/purnavr/roboshop-infra.git" },
-#    { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/frontend.git" },
+    { name = "roboshop", folder = "infra", repo_url = "https://github.com/purnavr/roboshop-infra.git" },
+    { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/frontend.git" },
     { name = "catalogue", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/catalogue.git" },
-#    { name = "user", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/user.git" },
-#    { name = "cart", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/cart.git" },
-#    { name = "shipping", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/shipping.git" },
-#    { name = "payment", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/payment.git" },
+    { name = "user", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/user.git" },
+    { name = "cart", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/cart.git" },
+    { name = "shipping", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/shipping.git" },
+    { name = "payment", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/payment.git" },
   ]
 }
