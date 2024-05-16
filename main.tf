00776e23aede6b348cfae558de134bd850fa063c
purna <purnaraomba@gmail.com>
2024-05-16 21:25:51 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -4,15 +4,31 @@ resource "jenkins_folder" "folders" {
   name = var.folders[count.index]
 }
 
-resource "jenkins_job" "job" {
+#resource "jenkins_job" "job" {
+#  depends_on = [jenkins_folder.folders]
+#
+#  count = length(var.jobs)
+#  name     = lookup(element(var.jobs, count.index ), "name", null )
+#  folder   = lookup(element(var.jobs, count.index ), "folder", null )
+#  template = templatefile("${path.module}/sb-job.xml", {
+#    repo_url = lookup(element(var.jobs, count.index ), "repo_url", null )
+#  })
+#}
+
+resource "jenkins_job" "jobs" {
   depends_on = [jenkins_folder.folders]
 
   count = length(var.jobs)
-  name     = lookup(element(var.jobs, count.index ), "name", null )
-  folder   = lookup(element(var.jobs, count.index ), "folder", null )
+  name     = lookup(element(var.jobs, count.index), "name", null)
+  folder   = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
+
   template = templatefile("${path.module}/sb-job.xml", {
-    repo_url = lookup(element(var.jobs, count.index ), "repo_url", null )
+    repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
   })
+
+#  lifecycle {
+#    ignore_changes = [template]
+#  }
 }
 
 #resource "jenkins_job" "job" {
