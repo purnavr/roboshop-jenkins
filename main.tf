330fd93c6bb1cfb626312a2f13d4c83ea154e39e
purna <purnaraomba@gmail.com>
2024-05-16 16:16:49 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "jenkins_job" "job" {
   count = length(var.jobs)
   name     = var.jobs[count.index].name
   folder   = "/job/${lookup(element(var.jobs, count.index ), "folder", null )}"
-  template = templatefile("${path.module}/sb-jobs.xml", {
+  template = templatefile("${path.module}/sb-job.xml", {
     repo_url = var.jobs[count.index].repo_url
     name = var.jobs[count.index].name
   })
