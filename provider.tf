92e4a8943931962694ec5cc88bd27432a2aa18af
purna <purnaraomba@gmail.com>
2024-05-17 09:37:09 +0530

    Roboshop Update

--- provider.tf
+++ provider.tf
@@ -1,21 +1,34 @@
-## Configure the Jenkins Provider
+terraform {
+  required_providers {
+    jenkins = {
+      source = "taiidani/jenkins"
+    }
+  }
+}
+
+provider "jenkins" {
+  server_url = "http://54.90.237.42:8080/"
+  username   = data.aws_ssm_parameter.jenkins_user.value
+  password   = data.aws_ssm_parameter.jenkins_pass.value
+}
+
+data "aws_ssm_parameter" "jenkins_user" {
+  name = "jenkins.user"
+}
+
+data "aws_ssm_parameter" "jenkins_pass" {
+  name = "jenkins.pass"
+}
+
+
 #terraform {
 #  required_providers {
 #    jenkins = {
-#      source = "taiidani/jenkins"
+#      source = "registry.terraform.io/taiidani/jenkins"
 #    }
 #  }
 #}
-#
-##terraform {
-##  required_providers {
-##    jenkins = {
-##      source = "registry.terraform.io/taiidani/jenkins"
-##    }
-##  }
-##}
-#
-#
+
 #provider "jenkins" {
 #  server_url = "http://34.238.142.237:8080/"
 #  username   = data.aws_ssm_parameter.jenkins_user.value
@@ -29,33 +42,3 @@
 #data "aws_ssm_parameter" "jenkins_pass" {
 #  name = "jenkins.pass"
 #}
-
-#terraform {
-#  required_providers {
-#    jenkins = {
-#      source = "registry.terraform.io/taiidani/jenkins"
-#    }
-#  }
-#}
-
-terraform {
-  required_providers {
-    jenkins = {
-      source = "taiidani/jenkins"
-    }
-  }
-}
-
-provider "jenkins" {
-  server_url = "http://54.81.212.235:8080/"
-  username   = data.aws_ssm_parameter.jenkins_user.value
-  password   = data.aws_ssm_parameter.jenkins_pass.value
-}
-
-data "aws_ssm_parameter" "jenkins_user" {
-  name = "jenkins.user"
-}
-
-data "aws_ssm_parameter" "jenkins_pass" {
-  name = "jenkins.pass"
-}
