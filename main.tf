4449cd743174410a569f1848e2f5dc71bda365de
purna <purnaraomba@gmail.com>
2024-05-17 08:12:12 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -1,20 +1,53 @@
+#resource "jenkins_folder" "folders" {
+#  count = length(var.folders)
+#  name = var.folders[count.index]
+#}
+#
+#
+#
+#
+#resource "jenkins_job" "job" {
+#  depends_on = [jenkins_folder.folders]
+#
+#  count = length(var.jobs)
+#  name     = var.jobs[count.index].name
+#  folder   = "/job/${lookup(element(var.jobs, count.index ), "folder", null )}"
+#  template = templatefile("${path.module}/sb-job.xml", {
+#    repo_url = var.jobs[count.index].repo_url
+#    name = var.jobs[count.index].name
+#  })
+#
+#  lifecycle {
+#    ignore_changes = [template]
+#  }
+#}
+#
+#data "aws_instance" "jenkins" {
+#  instance_id = "i-0c3412def8affb72e"
+#}
+#
+#resource "aws_route53_record" "www" {
+#  zone_id = "Z01280802SKTCPOFIGWX3"
+#  name    = "jenkins.roboz.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [data.aws_instance.jenkins.public_ip]
+#}
+
 resource "jenkins_folder" "folders" {
   count = length(var.folders)
-  name = var.folders[count.index]
+  name = element(var.folders, count.index)
 }
 
-
-
-
-resource "jenkins_job" "job" {
+resource "jenkins_job" "jobs" {
   depends_on = [jenkins_folder.folders]
 
   count = length(var.jobs)
-  name     = var.jobs[count.index].name
-  folder   = "/job/${lookup(element(var.jobs, count.index ), "folder", null )}"
+  name     = lookup(element(var.jobs, count.index), "name", null)
+  folder   = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
+
   template = templatefile("${path.module}/sb-job.xml", {
-    repo_url = var.jobs[count.index].repo_url
-    name = var.jobs[count.index].name
+    repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
   })
 
   lifecycle {
@@ -23,12 +56,12 @@ resource "jenkins_job" "job" {
 }
 
 data "aws_instance" "jenkins" {
-  instance_id = "i-0c3412def8affb72e"
+  instance_id = "i-09e791412076778a9"
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = "Z01280802SKTCPOFIGWX3"
-  name    = "jenkins.roboz.online"
+  zone_id = "Z0519871SX8ZUH6ORUV5"
+  name    = "jenkins.devtb.online"
   type    = "A"
   ttl     = 30
   records = [data.aws_instance.jenkins.public_ip]
--- provider.tf
+++ provider.tf
@@ -1,23 +1,45 @@
-# Configure the Jenkins Provider
+## Configure the Jenkins Provider
+#terraform {
+#  required_providers {
+#    jenkins = {
+#      source = "taiidani/jenkins"
+#    }
+#  }
+#}
+#
+##terraform {
+##  required_providers {
+##    jenkins = {
+##      source = "registry.terraform.io/taiidani/jenkins"
+##    }
+##  }
+##}
+#
+#
+#provider "jenkins" {
+#  server_url = "http://34.238.142.237:8080/"
+#  username   = data.aws_ssm_parameter.jenkins_user.value
+#  password   = data.aws_ssm_parameter.jenkins_pass.value
+#}
+#
+#data "aws_ssm_parameter" "jenkins_user" {
+#  name = "jenkins.user"
+#}
+#
+#data "aws_ssm_parameter" "jenkins_pass" {
+#  name = "jenkins.pass"
+#}
+
 terraform {
   required_providers {
     jenkins = {
-      source = "taiidani/jenkins"
+      source = "registry.terraform.io/taiidani/jenkins"
     }
   }
 }
 
-#terraform {
-#  required_providers {
-#    jenkins = {
-#      source = "registry.terraform.io/taiidani/jenkins"
-#    }
-#  }
-#}
-
-
 provider "jenkins" {
-  server_url = "http://34.238.142.237:8080/"
+  server_url = "http://54.211.168.236:8080/"
   username   = data.aws_ssm_parameter.jenkins_user.value
   password   = data.aws_ssm_parameter.jenkins_pass.value
 }
