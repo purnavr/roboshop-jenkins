87b844492eb5b854595fd90ad3f409adc7ded388
purna <purnaraomba@gmail.com>
2024-05-21 21:01:22 +0530

    Roboshop Update

--- vars.tf
+++ vars.tf
@@ -4,7 +4,6 @@ variable "folders" {
 
 variable "m-jobs" {
   default = [
-    { name = "roboshop", folder = "infra", repo_url = "https://github.com/purnavr/roboshop-infra.git" },
     { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/frontend.git" },
     { name = "catalogue", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/catalogue.git" },
     { name = "user", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/user.git" },
