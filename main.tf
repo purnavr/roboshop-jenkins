8169999789f07edeb6803bbf4881364bff366bd9
purna <purnaraomba@gmail.com>
2024-05-21 20:55:48 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "jenkins_folder" "folders" {
   name = var.folders[count.index]
 }
 
-resource "jenkins_job" "job" {
+resource "jenkins_job" "s-job" {
   depends_on = [jenkins_folder.folders]
 
   count = length(var.jobs)
@@ -12,6 +12,22 @@ resource "jenkins_job" "job" {
   template = templatefile("${path.module}/sb-job.xml", {
     repo_url = var.jobs[count.index].repo_url
     name = var.jobs[count.index].name
+  })
+
+  lifecycle {
+    ignore_changes = [template]
+  }
+}
+
+resource "jenkins_job" "m-job" {
+  depends_on = [jenkins_folder.folders]
+
+  count = length(var.m-jobs)
+  name     = var.m-jobs[count.index].name
+  folder   = "/job/${lookup(element(var.m-jobs, count.index ), "folder", null )}"
+  template = templatefile("${path.module}/mb-job.xml", {
+    repo_url = var.m-jobs[count.index].repo_url
+    name = var.m-jobs[count.index].name
   })
 
   lifecycle {
--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "folders" {
   default = ["infra", "CI-pipeline"]
 }
 
-variable "jobs" {
+variable "m-jobs" {
   default = [
     { name = "roboshop", folder = "infra", repo_url = "https://github.com/purnavr/roboshop-infra.git" },
     { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/frontend.git" },
@@ -12,4 +12,8 @@ variable "jobs" {
     { name = "shipping", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/shipping.git" },
     { name = "payment", folder = "CI-pipeline", repo_url = "https://github.com/purnavr/payment.git" },
   ]
+}
+
+variable "s-jobs" {
+  default = []
 }
