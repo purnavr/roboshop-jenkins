f0003ca7288172c4cbb73ed5a5184f42a6ef7d32
purna <purnaraomba@gmail.com>
2024-07-27 13:08:33 +0530

    Roboshop Update

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "jenkins" {
-  server_url = "http://23.20.236.249:8080/"
+  server_url = "http://3.85.96.139:8080/"
   username   = data.aws_ssm_parameter.jenkins_user.value
   password   = data.aws_ssm_parameter.jenkins_pass.value
 }
